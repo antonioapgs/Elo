b130e54094b91398b851838b060a01fb45574980
antonioapgs <antonioapgs@hotmail.com>
2018-08-20 22:13:10 -0300

    Criando estrutura básica do projeto.

---
+++ Elo.Business.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Elo.Business.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ Elo.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Elo.Business
+{
+    public class Class1
+    {
+    }
+}
